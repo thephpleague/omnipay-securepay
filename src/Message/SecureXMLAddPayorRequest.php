5635d71b34d258cbb4b08bb4228d9fcf0f9bd461
Mischa King <php@carpetcall.com.au>
2020-11-16 11:49:14 +1000

    Updated Add Payor Request

--- src/Message/SecureXMLAddPayorRequest.php
+++ src/Message/SecureXMLAddPayorRequest.php
@@ -10,16 +10,20 @@ namespace Omnipay\SecurePay\Message;
  */
 class SecureXMLAddPayorRequest extends SecureXMLAbstractRequest
 {
-    protected $actionType = 'add'; 
+    protected $actionType = 'add';
     protected $requiredFields = array('amount', 'card', 'currency');
 
     public function getData()
     {
         $xml = $this->getBasePaymentXML();
 
-       	$xml = $this->getCardXML($xml);
+        $card = $xml->Periodic->PeriodicList->PeriodicItem->addChild('CreditCardInfo');
+        $card->addChild('cardNumber', $this->getCard()->getNumber());
+        $card->addChild('cvv', $this->getCard()->getCvv());
+        $card->addChild('expiryDate', $this->getCard()->getExpiryDate('m/y'));
 
         $xml->Periodic->PeriodicList->PeriodicItem->addChild('periodicType', 4);
+        $xml->Periodic->PeriodicList->PeriodicItem->actionType = 'add';
 
         return $xml;
     }
